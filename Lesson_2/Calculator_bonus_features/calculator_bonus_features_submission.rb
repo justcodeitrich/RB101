314a6bc08de78ea659a0d6dedc89c1cae443a092
Richard Choi <justcodeitrich@gmail.com>
2021-08-31 16:35:01 -0400

    Updates made - another calculation prompt, cleaned up unnecessary parenthesis and Kernel, refactored if statements, added to YAML files

--- Lesson_2/Calculator_bonus_features/calculator_bonus_features_submission.rb
+++ Lesson_2/Calculator_bonus_features/calculator_bonus_features_submission.rb
@@ -1,15 +1,10 @@
-# Bonus feature 1 - lines 15 - 17
-# Bonus feature 2 - lines 19 - 21
-# Bonus feature 3 - line 34
-# Bonus feature 4 - line 7 and related YAML files
-# Bonus feature 5 - lines 8 - 9 and throughout code
 
 require 'yaml'
 MESSAGES = YAML.load_file('calculator_messages.yml')
 SPANISH_MESSAGES = YAML.load_file('spanish_calculator_messages.yml')
 
 def prompt(message)
-  Kernel.puts("=> #{message}")
+  puts "=> #{message}"
 end
 
 def integer?(num)
@@ -37,7 +32,7 @@ prompt(MESSAGES['language_select'])
 
 language_preference = nil
 loop do
-  language_preference = Kernel.gets().chomp()
+  language_preference = gets.chomp
   break if language_preference == '1' || language_preference == '2'
   prompt("Please type 1 for English or type 2 for Spanish")
 end
@@ -55,9 +50,9 @@ prompt(LANGUAGE['welcome'])
 name = ''
 
 loop do
-  name = Kernel.gets().chomp()
+  name = gets.chomp
 
-  break unless name.empty?()
+  break unless name.empty?
   prompt(LANGUAGE['valid_name'])
 end
 
@@ -70,7 +65,7 @@ loop do
 
   loop do
     prompt(LANGUAGE['ask_first_number'])
-    number1 = Kernel.gets().chomp()
+    number1 = gets.chomp
 
     break if number?(number1)
     prompt(LANGUAGE['valid_number'])
@@ -78,7 +73,7 @@ loop do
 
   loop do
     prompt(LANGUAGE['ask_second_number'])
-    number2 = Kernel.gets().chomp()
+    number2 = gets.chomp
 
     break if number?(number2)
     prompt(LANGUAGE['valid_number'])
@@ -89,7 +84,7 @@ loop do
   prompt(LANGUAGE['operator_prompt'])
   operator = ''
   loop do
-    operator = Kernel.gets().chomp()
+    operator = gets.chomp
 
     break if %w(1 2 3 4).include?(operator)
     prompt(LANGUAGE["operator_selection_error"])
@@ -102,37 +97,45 @@ loop do
   result = case operator
            when '1'
              if number1.include?('.') || number2.include?('.')
-               number1.to_f() + number2.to_f()
+               number1.to_f + number2.to_f
              else
-               number1.to_i() + number2.to_i()
+               number1.to_i + number2.to_i
              end
            when '2'
              if number1.include?('.') || number2.include?('.')
-               number1.to_f() - number2.to_f()
+               number1.to_f - number2.to_f
              else
-               number1.to_i() - number2.to_i()
+               number1.to_i - number2.to_i
              end
            when '3'
              if number1.include?('.') || number2.include?('.')
-               number1.to_f() * number2.to_f()
+               number1.to_f * number2.to_f
              else
-               number1.to_i() * number2.to_i()
+               number1.to_i * number2.to_i
              end
            when '4'
-             if number1.to_f() == 0.0 || number2.to_f() == 0.0
+             if number1.to_f == 0.0 || number2.to_f == 0.0
                "undefined as a result of division by 0"
              else
-               number1.to_f() / number2.to_f()
+               number1.to_f / number2.to_f
              end
            end
   prompt(LANGUAGE['result'] + " #{result}.")
 
   prompt(LANGUAGE['do_it_again?'])
-  answer = Kernel.gets().chomp()
-  break unless answer.downcase().start_with?('y')
-  system('clear')
+
+   
+  answer = ''
+  loop do
+    answer = gets.chomp
+    if answer.downcase == 'y' || answer.downcase == 'yes'
+      break
+    elsif answer.downcase == 'n' || answer.downcase == 'no'
+      prompt(LANGUAGE['goodbye_message'])
+      break 
+    else
+      prompt(LANGUAGE['again_error'])
+    end
+  end
+  break if answer.downcase == 'n' || answer.downcase == 'no'
 end
-
-prompt(LANGUAGE['goodbye_message'])
-
-system('clear')
