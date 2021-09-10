00f32924e9fbf77cd7f60ce2b4ff8547b4a59ae2
Richard Choi <justcodeitrich@gmail.com>
2021-09-10 17:26:33 -0400

    updated heredoc and calculate_again? method using feedback

--- Lesson_2/mortgage_calculator/mortgage_calculator.rb
+++ Lesson_2/mortgage_calculator/mortgage_calculator.rb
@@ -80,10 +80,10 @@ def get_mnth_pay(loan_amt, mnth_int_rate, mnth_loan_dura)
   (loan_amt) * (mnth_int_rate / (1 - (1 + mnth_int_rate)**(-mnth_loan_dura)))
 end
 
+VALID_YES_NO = ['yes','y','no','n']
 def calculate_again?
-  go_again = gets.chomp.downcase
-  loop do
-    break if go_again.start_with?('y') || go_again.start_with?('n')
+  go_again = gets.chomp.downcase.strip
+  until VALID_YES_NO.include?(go_again)
     prompt(MESSAGES['calculate_again_error'])
     go_again = gets.chomp.downcase
   end
@@ -92,6 +92,14 @@ end
 
 def clear_screen
   system('clear')
+end
+
+def display_results(monthly_pay,monthly_interest_rate,monthly_loan_duration)
+  results_message = <<~MSG
+  Your monthly payment is $#{monthly_pay.round(2)}
+  >>> Your monthly interest rate is #{(monthly_interest_rate * 100).round(2)}%
+  >>> Your monthly loan duration is #{monthly_loan_duration} months
+  MSG
 end
 
 clear_screen
@@ -107,14 +115,7 @@ loop do
   mnth_int_rate = ((user_input_apr.to_f / 100) / 12)
   mnth_loan_dura = duration_in_months(user_input_loan_duration)
   mnth_pay = get_mnth_pay(loan_amount, mnth_int_rate, mnth_loan_dura)
-
-  results_message = <<~MSG
-  Your monthly payment is $#{mnth_pay.round(2)}
-  >>> Your monthly interest rate is #{(mnth_int_rate * 100).round(2)}%
-  >>> Your monthly loan duration is #{mnth_loan_dura} months
-  MSG
-
-  prompt(results_message)
+  prompt(display_results(mnth_pay,mnth_int_rate,mnth_loan_dura))
   prompt(MESSAGES['again?'])
   break if calculate_again?.start_with?('n')
   clear_screen
