185acd8248850fb86f1a2c9471fde49425c4ceb5
Richard Choi <justcodeitrich@gmail.com>
2021-09-25 15:11:30 -0400

    rubocop first cleanup

--- Lesson_2/rock_paper_scissors/rock_paper_scissors.rb
+++ Lesson_2/rock_paper_scissors/rock_paper_scissors.rb
@@ -1,31 +1,24 @@
 require 'yaml'
 MESSAGES = YAML.load_file('text.yml')
 
-VALID_CHOICES = ['rock', 'paper', 'scissors', 'spock', 'lizard', 'r','p','sc','sp','l']
-VALID_YES_NO = ['y','yes','n','no']
+VALID_CHOICES = ['rock', 'paper', 'scissors', 'spock', 'lizard', 'r', 'p', 'sc', 'sp', 'l']
+VALID_YES_NO = ['y', 'yes', 'n', 'no']
 
 def prompt(message)
   puts("=> #{message}")
 end
 
-# spock beats rock & scissors
-# lizard beats spock & paper
-# rock beats lizard and scissors
-# paper beats rock & spock
-# scissors beats lizard & paper
+def win?(p1, p2)
+  options = {
+    spock: ['rock', 'scissors'],
+    lizard: ['spock', 'paper'],
+    rock: ['lizard', 'scissors'],
+    paper: ['rock', 'spock'],
+    scissors: ['lizard', 'paper']
+  }
+  p1 = p1.to_sym
 
-def win?(first, second)
-  
-  win_conditions = {
-  spock: ['rock', 'scissors'],
-  lizard: ['spock', 'paper'],
-  rock: ['lizard', 'scissors'],
-  paper: ['rock', 'spock'],
-  scissors: ['lizard', 'paper']
-}
-  first = first.to_sym
-
-  win_conditions[first] && ("#{second}" == win_conditions[first][0] || "#{second}" == win_conditions[first][1])
+  options[p1] && (p2.to_s == options[p1][0] || p2.to_s == options[p1][1])
 end
 
 def display_results(player, computer)
@@ -42,33 +35,32 @@ def clear_screen
   system('clear')
 end
 
-def validate_choice(choice)
-  case 
-    when choice == 'r' 
-      'rock'
-    when choice == 'p' 
-      'paper'
-    when choice == 'sc'  
-      'scissors'
-    when choice == 'l'  
-      'lizard'
-    when choice == 'sp'  
-      'spock'
-    else choice
+def validate_choice(input)
+  case
+  when input == 'r'
+    'rock'
+  when input == 'p'
+    'paper'
+  when input == 'sc'
+    'scissors'
+  when input == 'l'
+    'lizard'
+  when input == 'sp'
+    'spock'
+  else input
   end
 end
 
-def add_score(result,score_count)
+def add_score(result, scoreboard)
   if result.include?('You')
-    score_count[:player_score] += 1
+    scoreboard[:p1] += 1
   elsif result.include?('Computer')
-    score_count[:computer_score] += 1
-  else
+    scoreboard[:comp] += 1
   end
 end
 
-def declare_winner(score_count)
-  if score_count[:player_score] == 3
+def declare_winner(scoreboard)
+  if scoreboard[:p1] == 3
     prompt(MESSAGES['player_wins'])
   else
     prompt(MESSAGES['computer_wins'])
@@ -92,18 +84,17 @@ def play_again?
   go_again[0] == 'y'
 end
 
-score_count = {
-  player_score: 0,
-  computer_score: 0
-  }
+scoreboard = {
+  p1: 0,
+  comp: 0
+}
 
 welcome_player
 
 loop do
   choice = ''
- 
+
   loop do
-    
     prompt(MESSAGES['ask_user_for_selection'])
     choice = gets.chomp.downcase.strip
     if VALID_CHOICES.include?(choice)
@@ -120,31 +111,32 @@ loop do
   prompt("You chose: #{choice}. Computer chose: #{computer_choice}")
 
   result = display_results(choice, computer_choice)
-  
+
   prompt(result)
   prompt(MESSAGES['next_round'])
 
-  until gets.chomp != nil
+  until !gets.chomp.nil?
   end
 
   clear_screen
-  add_score(result,score_count)
+  add_score(result, scoreboard)
 
-  prompt("Your score: #{score_count[:player_score]} | Computer score: #{score_count[:computer_score]}")
-  
-  next unless score_count[:player_score] == 3 || score_count[:computer_score] == 3
+  prompt("Your score: #{scoreboard[:p1]} | Computer score: #{scoreboard[:comp]}")
+
+  next unless scoreboard[:p1] == 3 || scoreboard[:comp] == 3
   clear_screen
 
-  declare_winner(score_count)
+  declare_winner(scoreboard)
 
   prompt(MESSAGES['play_again?'])
   break unless play_again?
-  
+
+  scoreboard = {
+    p1: 0,
+    comp: 0
+  }
+
   clear_screen
-  score_count = {
-    player_score: 0,
-    computer_score: 0
-    }
 end
 
 clear_screen
