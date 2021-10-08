ccd77f175dacc24b0b52c4141fb1464996776177
Richard Choi <justcodeitrich@gmail.com>
2021-10-08 15:46:34 -0400

    deleted small problems from Lesson 2

--- Lesson_2/small_problems/easy_1/1.rb
+++
@@ -1,6 +0,0 @@
-# Difference between file::path and file#path?
-# file ::path - public class method - returns the string representation of the path
-# file #path - public instance method - returns the pathname used to create file as a string. Does not normalize the name.
-
-
-
