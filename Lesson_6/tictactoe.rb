d4d3546f66a05d92f2687150b5b214de47839d20
Richard Choi <justcodeitrich@gmail.com>
2021-11-06 15:01:29 -0400

    bonus feature 5a and 5b

--- Lesson_6/tictactoe.rb
+++ Lesson_6/tictactoe.rb
@@ -67,7 +67,7 @@ WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
                 [[1, 5, 9], [3, 5, 7]] # diagonals
 
-DEFENSE_LINES = { 
+ACTION_PAIRS = { 
   [1,2] => 3, 
   [2,3] => 1, 
   [4,5] => 6, 
@@ -86,64 +86,63 @@ DEFENSE_LINES = {
   [5,7] => 3  
 }
 
+def comp_offense_defense(pairs,brd,sqr)
+
+  if pairs.length > 0 && sqr == nil
+    pairs.keys.each do |pair_value|
+        value = ACTION_PAIRS[pair_value]
+        return value if empty_squares(brd).include?(value)
+      end
+  end
+  sqr
+end
+
 def computer_places_piece!(brd)
   square = nil
 
-  player_pairs = DEFENSE_LINES.select do |line|
+  player_pairs = ACTION_PAIRS.select do |line|
     brd.values_at(line[0],line[1]).count(PLAYER_MARKER) == 2
   end
 
-  computer_pairs = DEFENSE_LINES.select do |line|
+  computer_pairs = ACTION_PAIRS.select do |line|
     brd.values_at(line[0],line[1]).count(COMPUTER_MARKER) == 2
   end
 
-  if player_pairs.empty? && computer_pairs.empty? # select randomly if both player & computer have no pairs
-    square = empty_squares(brd).sample
-    # puts "from both empty"
-    return brd[square] = COMPUTER_MARKER
-  end
-
-  # puts "player pairs #{player_pairs}"
-  # puts "computer pairs #{computer_pairs}"
-  # puts "empty squares #{empty_squares(brd)}"
-
   # OFFENSE first
-  if computer_pairs.length > 0
-    # puts "Went Offense"
-    computer_pairs.keys.each do |pair_value|
-      value = DEFENSE_LINES[pair_value]
-      if empty_squares(brd).include?(value)
-        return brd[value] = COMPUTER_MARKER
-        # puts "Returned out from empty_squares"
-      end
-    end
-  end
-
+  square = comp_offense_defense(computer_pairs,brd,square)
+  p square
   # DEFENSE second
-  if player_pairs.length > 0
-    # puts "Went Defense"
-    player_pairs.keys.each do |pair_value|
-      value = DEFENSE_LINES[pair_value]
-      if empty_squares(brd).include?(value)
-        # puts "computer choice inside: #{value}"
-        return brd[value] = COMPUTER_MARKER
-        # puts "Picked from DEFENSE empty_squares"
-      end
-    end
-  end
+  square = comp_offense_defense(player_pairs,brd,square)
+  p square
+  # Pick 5 
+  square = pick_square_5(brd,square)
+  p square
+  # Pick randomly
+  square = empty_squares(brd).sample if square == nil
+  p square
 
-  square = empty_squares(brd).sample
-  # puts "computer choice random: #{square}"
   brd[square] = COMPUTER_MARKER
 end
 
-# add-on to the previous defense to make the computer offensive too
-# if the computer attacks, then it wont defend. It should attack once it considers the player's recently played moved
-# aka defend first, if no impending risk then attack.
-# bug - after the 3rd player input, computer stops placing pieces and player keeps going
-  # this only happens when a tie is imminent with 4 player pieces and 3 computer pieces.
-  # It's not registering that it can make a move
-# What happens when player places piece down on a square and the computer wants to put its piece there to complete a row?
+# objective: have computer pick square 5 if it's available
+# pick offensive move, then defensive move, then square 5, then random
+# sub-project: square 5
+  # write a method that has computer place piece on square 5 if it is available
+  def pick_square_5(brd,sqr)
+    if empty_squares(brd).include?(5) && sqr == nil
+      5
+    else 
+      sqr
+    end
+  end
+
+  # refactor the offense and defense moves into methods
+  # use a turn counter to increment each turn
+
+# ultimately the computer_places_piece! is to mutate the board hash that gets passed in
+# brd [square] = COMPUTER_MARKER
+# therefore the square value needs to be able to move through each helper method
+# offense, defense, square 5, random
 
 
 def board_full?(brd)
