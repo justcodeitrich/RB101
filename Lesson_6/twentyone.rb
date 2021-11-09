4f308e6a3642283472390c9d94cd04a0cb7af4c7
Richard Choi <justcodeitrich@gmail.com>
2021-11-09 17:19:15 -0500

    PEDAC step 3 - player turn

--- Lesson_6/twentyone.rb
+++ Lesson_6/twentyone.rb
@@ -68,8 +68,15 @@
         
     # 3. Player turn: hit or stay
     #   - repeat until bust or "stay"
+      # initialize a method that totals the values of cards selected
+        # input: value of player_hand (a nested array)
+        # output: integer
       # Initialize a method to determine Ace value throughout the game
+        # input: integer representing player's hand total value
+        # output: 1 or 11
+          # note: it is possible to draw two aces as first hand, that needs to be accounted for
       # Use a loop
+
     # 4. If player bust, dealer wins.
     # 5. Dealer turn: hit or stay
     #   - repeat until total >= 17
@@ -94,34 +101,96 @@
     deck
   end
 
-  deck = initialize_deck
-
-
-# 2. Deal cards to player and dealer
-  # Initialize a "hand" for player and dealer
-    # initialize a method deal_hand
-      #input: deck
-      #output: an array of two sub-arrays from deck
-    # use Array#sample to select a sub-array within the deck
-    # once this sub-array(card) has been drawn, remove it from the deck
-      #initialize method remove_card
-        #input: selected sub-array
-        #output: mutated deck without the selected sub-array
-
   def deal_hand(deck)
   hand = []
   card = nil
     2.times do 
       card = deck.sample
       hand << card
-      remove_card(deck,card)
+      remove_card!(deck,card)
     end
     hand
   end
 
-  def remove_card(deck,card)
+  def remove_card!(deck,card)
     deck.delete(card)
   end
 
-  player_hand = deal_hand(deck)
-  dealer_hand = deal_hand(deck)
+  # 3. Player turn: hit or stay
+#   - repeat until bust or "stay"
+  # initialize a method hit!
+    #input: deck
+    #output: sub-array from deck
+      #side effect - mutates deck by removing the selected card
+
+  # initialize a method that totals the values of cards selected
+    # input: value of player_hand (a nested array)
+    # output: integer
+  # Initialize a method to determine Ace value throughout the game
+    # input: integer representing player's hand total value
+    # output: 1 or 11
+  # Use a loop
+    # prompt user with their hand total
+    # prompt user to hit or stay
+      # take response and hit! if hit or break out of loop if stay
+    # check to see if bust - if bust, prompt user and skip to end of game
+
+
+def bust?(total_hand_value)
+  true if total_hand_value > 21
+end
+
+def prompt(msg)
+  puts "#=> #{msg}"
+end
+
+def hit!(deck,hand)
+  card = deck.sample
+  hand << card
+  remove_card!(deck,card)
+  card
+end
+
+def total_hand_value(hand)
+  value = 0
+  hand.each do |card|
+    case 
+      when card.last == 'ace' then value += ace_value(value)
+      when card.last == 'jack' then value += 10
+      when card.last == 'queen' then value += 10
+      when card.last == 'king' then value += 10
+      else value += card.last
+    end
+  end
+  value
+end
+
+def ace_value(value)
+  if (value + 11) > 21
+    1
+  else
+    11
+  end
+end
+
+deck = initialize_deck
+
+player_hand = deal_hand(deck)
+dealer_hand = deal_hand(deck)
+def player_sequence(player_hand,deck)
+  loop do
+    prompt "Your hand is #{player_hand}."
+    prompt "Your total value is #{total_hand_value(player_hand)}"
+    prompt "Do you want to hit or stay?"
+    answer = gets.chomp.downcase
+    if answer == 'hit'
+      hit!(deck,player_hand)
+    elsif answer == 'stay'
+      puts "STAY"
+      break
+    end
+    break puts 'BUST' if bust?(total_hand_value(player_hand))
+  end
+end
+
+player_sequence(player_hand,deck)
